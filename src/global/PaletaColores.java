15728de6ced4d9bde6284eb5ca182510870df05d
elsoftpy <57192394+elsoftpy@users.noreply.github.com>
2023-06-17 12:01:30 -0400

    paleta de colores

--- src/global/PaletaColores.java
+++ src/global/PaletaColores.java
@@ -12,4 +12,8 @@ import java.awt.Color;
  */
 public class PaletaColores {
     public static final Color salmon = new Color(242, 135, 117);
+    public static final Color peach = new Color(247, 194, 137);
+    public static final Color corn = new Color(234, 228, 143);
+    public static final Color pale = new Color(249, 217, 192);
+    public static final Color linen = new Color(249, 246, 228);
 }
